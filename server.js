af98bbd0d2a15d6c2a72896fca0d1c164f995741
lexrayne <lexrayne@outlook.com>
2023-07-15 23:54:00 -0300

    Added utils for file writing

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -21,5 +25,12 @@ app.get('*', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET request for database
+app.get('/api/notes', (req, res) => {
+    res.json(`${req.method} request received to get data`);
+
+    console.info(`${req.method} request received to get data`);
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
