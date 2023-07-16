200e97c3b081c91be2baf1bdd705e5bed3a1fd5c
lexrayne <lexrayne@outlook.com>
2023-07-16 00:24:56 -0300

    Added POST route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -26,11 +28,31 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET request for database
+// GET route for database
 app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to get data`);
-
     console.info(`${req.method} request received to get data`);
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+// POST route adding to database
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        }
+
+        readAndAppend(newNote, './db/notes.json');
+        res.json("Note added successfully");
+    } else {
+        res.error("Error in adding note");
+    }
+})
+
+app.listen(PORT, () =>
+    console.log(`App listening on port ${PORT}`)
+);
